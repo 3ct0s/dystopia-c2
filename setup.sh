41993b030fac784261b52b5f968f0c486db52247
Dimitris Kalopisis <74763463+3ct0s@users.noreply.github.com>
2022-01-17 16:51:49 +0200

    Added "-s" argument check
    Added "-s" argument check to install Python without GUI

--- setup.sh
+++ setup.sh
@@ -15,5 +15,12 @@ if test -f "$FILE"; then
 else
 	sudo wget https://www.python.org/ftp/python/3.8.9/python-3.8.9.exe
 fi
-sudo wine cmd /c python-3.8.9.exe /quiet InstallAllUsers=0
+arg1=$1
+arg2="-s"
+if [ "$arg1" == "$arg2" ]; then
+	echo "Beginning silent Python 3.8.9 Installation"
+	sudo wine cmd /c python-3.8.9.exe /quiet InstallAllUsers=0
+else
+	sudo wine cmd /c python-3.8.9.exe
+fi
 sudo wine "/root/.wine/drive_c/users/root/Local Settings/Application Data/Programs/Python/Python38-32/python.exe" -m pip install keyboard==0.13.5 pywin32==303 pycryptodome==3.12.0 pyautogui==0.9.53 pyinstaller==4.2 discord_webhook==0.14.0 discord.py
