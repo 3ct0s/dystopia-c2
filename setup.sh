f3d1fea7f40066270318208a4ddf32c43e7b753e
Dimitris Kalopisis <74763463+3ct0s@users.noreply.github.com>
2022-05-20 00:10:37 +0300

    fix: Pyinstaller Issue
    ModuleNotFoundError: No module named 'PyInstaller.__main__'; 'PyInstaller' is not a package : FIXED

--- setup.sh
+++ setup.sh
@@ -23,4 +23,4 @@ if [ "$arg1" == "$arg2" ]; then
 else
 	sudo wine cmd /c python-3.8.9.exe
 fi
-sudo wine "/root/.wine/drive_c/users/root/Local Settings/Application Data/Programs/Python/Python38-32/python.exe" -m pip install keyboard==0.13.5 pywin32==303 pycryptodome==3.12.0 pyautogui==0.9.53 pyinstaller==4.2 discord_webhook==0.14.0 discord.py opencv-python
+sudo wine "/root/.wine/drive_c/users/root/Local Settings/Application Data/Programs/Python/Python38-32/python.exe" -m pip install keyboard==0.13.5 pywin32==303 pycryptodome==3.12.0 pyautogui==0.9.53 pyinstaller discord_webhook==0.14.0 discord.py opencv-python
